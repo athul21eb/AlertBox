be419acf93611a18b7a785a6d07417b34cfaea27
athul21eb <124911334+athul21eb@users.noreply.github.com>
2023-12-21 21:46:08 +0530

    corrected

---
+++ lib/home.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Alert Box"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          style: buttonStyle,
+          child: const Text(
+            "Show  AlertBox",
+            style: textStyle,
+          ),
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  backgroundColor: Colors.white,
+                  actions: [
+                    TextButton(
+                      style: buttonStyle,
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                        "Cancel",
+                        style: textStyle.copyWith(
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      style: buttonStyle,
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                        "OK",
+                        style: textStyle.copyWith(
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                  ],
+                  title: const Text(
+                    "Do you want to exit ?",
+                    style: textStyle,
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'constants.dart';
+import 'home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,74 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.dark,
       darkTheme: ThemeData.dark(),
       home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Alert Box"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: ElevatedButton(
-          style: buttonStyle,
-          child: const Text(
-            "Show  AlertBox",
-            style: textStyle,
-          ),
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  backgroundColor: Colors.white,
-                  actions: [
-                    TextButton(
-                      style: buttonStyle,
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: Text(
-                        "Cancel",
-                        style: textStyle.copyWith(
-                          color: Colors.blue,
-                        ),
-                      ),
-                    ),
-                    TextButton(
-                      style: buttonStyle,
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                        Navigator.of(context).pop();
-                      },
-                      child: Text(
-                        "OK",
-                        style: textStyle.copyWith(
-                          color: Colors.red,
-                        ),
-                      ),
-                    ),
-                  ],
-                  title: const Text(
-                    "Do you want to exit ?",
-                    style: textStyle,
-                  ),
-                );
-              },
-            );
-          },
-        ),
-      ),
     );
   }
 }
